7c7ca7c5852a1151c9a49b8ee70de496f4c09b3c
Matt Miller <thisismattmiller@gmail.com>
2021-06-27 17:07:13 -0400

    Added Whitenoise

--- ch_lod/settings/production.py
+++ ch_lod/settings/production.py
@@ -21,6 +21,18 @@ ALLOWED_HOSTS = ['*']
 
 DEBUG = True
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
+
+
+
+
 
 
 try:
